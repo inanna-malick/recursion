1a0b0a5489ec2f4667a4165a2c2a0ee612e94e95
Inanna Malick <inanna@recursion.wtf>
2022-07-07 13:00:51 -0700

    absolutely insane grep impl with lazy short circuit

--- src/examples/git.rs
+++ src/examples/git.rs
@@ -1,15 +1,19 @@
-use std::{collections::HashMap, default};
+use std::{collections::HashMap, fs};
+
+use futures::{future::BoxFuture, FutureExt};
 
 use crate::recursive::{Functor, Recursive, RecursiveStruct};
 
-struct Metadata{
-    xattrs: Vec<(String,String)>,
+pub struct Metadata {
+    pub xattrs: Vec<(String, String)>,
 }
+
+type PathComponent = String;
 
 // structure of the file tree with metadata, no file contents
 pub enum FileTree<A> {
     File(Metadata),
-    Dir(HashMap<String, A>),
+    Dir(HashMap<PathComponent, A>),
 }
 
 impl<A, B> Functor<B> for FileTree<A> {
@@ -30,7 +34,8 @@ impl<A, B> Functor<B> for FileTree<A> {
 pub type RecursiveFileTree = RecursiveStruct<FileTree<usize>>;
 
 pub struct GrepResult {
-    pub path: Vec<String>, // path components
+    pub path: Vec<PathComponent>, // path components
+    pub metadata: Metadata,
     pub contents: String,
 }
 
@@ -43,32 +48,59 @@ impl RecursiveFileTree {
     }
 
     // return vec of grep results
-    pub fn grep(self, substring: &str, ) -> Vec<GrepResult> {
-        self.cata(|node: FileTree<Vec<GrepResult>>| match node {
-            FileTree::File(contents) => {
-                if contents.contains(substring) {
-                    vec![GrepResult {
-                        path: Vec::new(),
-                        contents: contents.to_string(),
-                    }]
-                } else {
-                    Vec::new()
-                }
-            }
-            FileTree::Dir(xs) => xs
-                .into_iter()
-                .flat_map(|(path_segment, search_results)| {
-                    search_results.into_iter().map(move |grep_result| {
-                        let mut path = vec![path_segment.clone()];
-                        path.extend(grep_result.path);
-                        GrepResult {
-                            path,
-                            contents: grep_result.contents,
+    pub fn grep<'a, F: Fn(&[PathComponent]) -> bool + Send + Sync + 'a>(
+        self,
+        substring: &'a str,
+        filter: &'a F,
+    ) -> BoxFuture<'a, Vec<GrepResult>> {
+
+
+
+
+        // dear lord, this is absolutely atrocious
+        self.cata(
+            move |node: FileTree<
+                Box<dyn FnOnce(Vec<PathComponent>) -> BoxFuture<'a, Vec<GrepResult>> + Send + Sync>,
+            >| {
+                Box::new({
+                    move |path| {
+                        async move {
+                            match node {
+                                FileTree::File(metadata) => {
+                                    let contents = fs::read_to_string(path.join("/"))
+                                        .expect("todo short circuit here");
+                                    if contents.contains(substring) {
+                                        vec![GrepResult {
+                                            path,
+                                            metadata,
+                                            contents: contents,
+                                        }]
+                                    } else {
+                                        Vec::new()
+                                    }
+                                }
+                                FileTree::Dir(search_results_futs) => {
+                                    let mut all_results = Vec::new();
+                                    for (path_component, search_result_fut) in
+                                        search_results_futs.into_iter()
+                                    {
+                                        let mut child_path = path.clone();
+                                        child_path.push(path_component);
+                                        if filter(&child_path[..]) {
+                                            // only expand child search branch if filter fn returns true
+                                            let search_result = search_result_fut(child_path).await;
+                                            all_results.extend(search_result.into_iter());
+                                        }
+                                    }
+                                    all_results
+                                }
+                            }
                         }
-                    })
+                        .boxed()
+                    }
                 })
-                .collect(),
-        })
+            },
+        )(Vec::new())
     }
 }
 
