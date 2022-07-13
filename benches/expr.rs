99620b4f9a0bb742996c0dece342c50c4ab31071
Inanna Malick <inanna@recursion.wtf>
2022-07-13 15:03:56 -0700

    update benchmarks w/ direct function comparison over a large series of runs on exprs of different sizes

--- benches/expr.rs
+++ benches/expr.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, PlotConfiguration, AxisScale};
 use schemes::{
     examples::expr::{
         eval::{eval, naive_eval},
@@ -10,29 +10,51 @@ use schemes::{
     recursive::{CoRecursive, Recursive},
 };
 
-fn bench_fib(criterion: &mut Criterion) {
-    // build a Big Expression that is Pointless and Shitty
-    let big_expr = RecursiveExpr::unfold(17, |x| {
-        if x > 0 {
-            Expr::Add(x - 1, x - 1)
-        } else {
-            Expr::LiteralInt(0)
-        }
-    });
+fn bench_eval(criterion: &mut Criterion) {
+    let mut test_cases = Vec::new();
 
-    let boxed_big_expr = big_expr.as_ref().fold(|n| match n {
-        Expr::Add(a, b) => Box::new(ExprAST::Add(a, b)),
-        Expr::LiteralInt(x) => Box::new(ExprAST::LiteralInt(x)),
-        _ => unreachable!(),
-    });
+    // build some Big Expressions that are Pointless and Shitty
+    for depth in 0..18 {
+        let big_expr = RecursiveExpr::unfold(depth, |x| {
+            if x > 0 {
+                Expr::Add(x - 1, x - 1)
+            } else {
+                Expr::LiteralInt(0)
+            }
+        });
+
+        let boxed_big_expr = big_expr.as_ref().fold(|n| match n {
+            Expr::Add(a, b) => Box::new(ExprAST::Add(a, b)),
+            Expr::LiteralInt(x) => Box::new(ExprAST::LiteralInt(x)),
+            _ => unreachable!(),
+        });
+
+        test_cases.push((depth, big_expr, boxed_big_expr));
+    }
 
     let h = HashMap::new();
 
-    criterion.bench_function("eval boxed", |b| {
-        b.iter(|| naive_eval(&h, black_box(&boxed_big_expr)))
-    });
-    criterion.bench_function("eval fold", |b| b.iter(|| eval(&h, black_box(&big_expr))));
+    let mut group = criterion.benchmark_group("eval");
+
+    let plot_config = PlotConfiguration::default()
+        .summary_scale(AxisScale::Logarithmic);
+    group.plot_config(plot_config);
+
+    for (depth, big_expr, boxed_big_expr) in test_cases.into_iter() {
+        let elem_count = 2_usize.pow(depth);
+        group.bench_with_input(
+            BenchmarkId::new("boxed", elem_count),
+            &boxed_big_expr,
+            |b, expr|  b.iter(|| naive_eval(&h, &expr)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("fold", elem_count),
+            &big_expr,
+            |b, expr| b.iter(|| eval(&h, expr)),
+        );
+    }
+    group.finish();
 }
 
-criterion_group!(benches, bench_fib);
+criterion_group!(benches, bench_eval);
 criterion_main!(benches);
