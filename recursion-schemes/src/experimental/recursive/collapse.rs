bfe432db8d0e5db84285abe860819c45b7c2117e
Inanna Malick <inanna@recursion.wtf>
2023-09-26 23:35:56 -0700

    working via futures unordered, finally

---
+++ recursion-schemes/src/experimental/recursive/collapse.rs
@@ -0,0 +1,41 @@
+use std::sync::Arc;
+
+use futures::{future::BoxFuture, Future, FutureExt};
+
+use crate::{
+    experimental::frame::{expand_and_collapse_async, AsyncMappableFrame},
+    frame::{expand_and_collapse, MappableFrame},
+    recursive::collapse::Collapsable,
+};
+
+/// The ability to collapse a value into some output type, frame by frame
+pub trait CollapsableAsync: Collapsable<FrameToken = Self::AsyncFrameToken>
+where
+    Self: Sized,
+{
+    type AsyncFrameToken: AsyncMappableFrame;
+
+    /// defined on trait for convenience and to allow for optimized impls
+    fn collapse_frames_async<Out, E>(
+        self,
+        collapse_frame: impl Fn(
+                <Self::AsyncFrameToken as MappableFrame>::Frame<Out>,
+            ) -> BoxFuture<'static, Result<Out, E>>
+            + Send
+            + Sync
+            + 'static,
+    ) -> BoxFuture<'static, Result<Out, E>>
+    where
+        Self: Send + Sync + 'static,
+        Out: Send + Sync + 'static,
+        <Self::AsyncFrameToken as MappableFrame>::Frame<Self>: Send + Sync + 'static,
+        <Self::AsyncFrameToken as MappableFrame>::Frame<Out>: Send + Sync + 'static,
+        E: Send + Sync + 'static,
+    {
+        expand_and_collapse_async::<Self, Out, E, Self::AsyncFrameToken>(
+            self,
+            Arc::new(|seed| std::future::ready(Ok(Self::into_frame(seed))).boxed()),
+            Arc::new(collapse_frame),
+        )
+    }
+}
---
+++ recursion-schemes/src/experimental/recursive/expand.rs
@@ -0,0 +1,41 @@
+use std::sync::Arc;
+
+use futures::{future::BoxFuture, FutureExt};
+
+use crate::{
+    experimental::frame::{expand_and_collapse_async, AsyncMappableFrame},
+    frame::MappableFrame,
+    recursive::expand::Expandable,
+};
+
+pub trait ExpandableAsync: Expandable<FrameToken = Self::AsyncFrameToken>
+where
+    Self: Sized,
+{
+    type AsyncFrameToken: AsyncMappableFrame;
+
+    /// defined on trait for convenience and to allow for optimized impls
+    fn expand_frames_async<In, E>(
+        seed: In,
+        expand_frame: impl Fn(
+                In,
+            )
+                -> BoxFuture<'static, Result<<Self::AsyncFrameToken as MappableFrame>::Frame<In>, E>>
+            + Send
+            + Sync
+            + 'static,
+    ) -> BoxFuture<'static, Result<Self, E>>
+    where
+        Self: Send + Sync + 'static,
+        In: Send + Sync + 'static,
+        <Self::AsyncFrameToken as MappableFrame>::Frame<Self>: Send + Sync + 'static,
+        <Self::AsyncFrameToken as MappableFrame>::Frame<In>: Send + Sync + 'static,
+        E: Send + Sync + 'static,
+    {
+        expand_and_collapse_async::<In, Self, E, Self::AsyncFrameToken>(
+            seed,
+            Arc::new(expand_frame),
+            Arc::new(|frame| std::future::ready(Ok(Self::from_frame(frame))).boxed()),
+        )
+    }
+}
